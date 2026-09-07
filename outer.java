bd059083858b93e1bf006773c0a917ff4b0744ec
Priyadharshini-student2006 <priyaselvaraj009@gmail.com>
2026-09-07 12:36:16 +0530

    outer

--- outer.java
+++ outer.java
@@ -1,5 +1,17 @@
 //nested class or inner classes -class within a class
 class outer{
-class inner{}
+
+ void createinner(){
+inner i1=new inner();
+i1.add(12,23);
 }
-//outer.class outer$inner.class
+         class inner{
+         void add(int a,int b){
+         System.out.println(a+b);
+         }
+         }
+public static void main(String[] ar){
+outer o=new outer();
+o.createinner();
+}
+}
